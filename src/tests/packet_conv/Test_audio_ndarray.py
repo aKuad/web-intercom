97f1b0a9c4ac6b8988639618f6aa54b79c66abfc
aKuad <53811809+aKuad@users.noreply.github.com>
2024-04-19 23:59:43 +0900

    Mod: `audio_ndarray.py` Audio packet conversion module for only ndarray - separated from `audio.py`
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

---
+++ src/tests/packet_conv/Test_audio_ndarray.py
@@ -0,0 +1,64 @@
+# coding: UTF-8
+"""Tests for ``packet_conv/audio_ndarray.py``
+
+Test cases:
+  * Can convert audio data in ``ndarray`` with[out] custom bytes to bytes, and reverse convert
+
+Test steps:
+  1. Set current here
+  2. Run this
+
+Author:
+  aKuad
+
+"""
+
+# For import top layer module
+import sys
+from pathlib import Path
+sys.path.append(Path(__file__).absolute().parent.parent.parent.__str__())
+
+
+import unittest
+
+import numpy as np
+
+from modules.packet_conv import audio_ndarray
+import AUDIO_PARAM
+
+
+class Test_packet_conv_audio(unittest.TestCase):
+  def test_audio_packet_enc_dec_ndarray_ext(self):
+    aud_org = part_create_testdata_ndarray()
+    ext_org = bytes([1, 2, 3, 4])
+
+    packet = audio_ndarray.encode(aud_org, ext_org)
+    aud_prc, ext_prc = audio_ndarray.decode(packet)
+
+    self.assertTrue((aud_org == aud_prc).all())
+    self.assertEqual(ext_org, ext_prc)
+
+
+  def test_audio_packet_enc_dec_ndarray_noext(self):
+    aud_org = part_create_testdata_ndarray()
+    ext_org = bytes()
+
+    packet = audio_ndarray.encode(aud_org, ext_org)
+    aud_prc, ext_prc = audio_ndarray.decode(packet)
+
+    self.assertTrue((aud_org == aud_prc).all())
+    self.assertEqual(ext_org, ext_prc)
+
+
+def part_create_testdata_ndarray() -> np.ndarray:
+  x = np.arange(AUDIO_PARAM.SAMPLE_RATE)
+  y = np.sin(np.deg2rad(x)) * (2**(AUDIO_PARAM.ONE_SAMPLE_BYTES * 8 - 1) - 100)
+  #                           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #                           Increase amplitude
+  # 2**(BYTES * 8 - 1) : data type's max value, -1 for sign bit
+  # - 100              : make margin
+  return np.int16(y).reshape(-1, 1) # reshape for monaural audio data
+
+
+if __name__ == "__main__":
+  unittest.main(verbosity=2)
