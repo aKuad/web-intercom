603e9aea69d1ab7d6aeeab62eb380ed998da1690
aKuad <53811809+aKuad@users.noreply.github.com>
2024-07-25 22:55:08 +0900

    Add: Docstring `AUDIO_PARAM.py` was missing - purpose should be written
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/modules/packet_conv/AUDIO_PARAM.py
+++ src/modules/packet_conv/AUDIO_PARAM.py
@@ -1,4 +1,12 @@
 # coding: UTF-8
+"""Audio data parameters at this system
+
+It for avoid magic-number coding. And keep same parameters at all modules.
+
+Author:
+  aKuad
+
+"""
 
 SAMPLE_RATE = 44100
 DTYPE = "int16"
