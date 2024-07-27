83a4b67a22cc611704bae46559a1f4f1848628b8
aKuad <53811809+aKuad@users.noreply.github.com>
2024-07-27 16:06:57 +0900

    Add: `AUDIO_PARAM.js` for define audio params in this program
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

---
+++ src/static/packet_conv/AUDIO_PARAM.js
@@ -0,0 +1,12 @@
+/**
+ * @file Audio data parameters at this system
+ *
+ * It for avoid magic-number coding. And keep same parameters at all modules.
+ *
+ * @author aKuad
+ */
+
+
+export const SAMPLE_RATE = 44100;
+export const FRAME_DURATION_SEC = 0.1;
+export const CHANNELS = 1;
