9495c34cd16a9af46cda7ddf22fba1ebc9e10058
aKuad <53811809+aKuad@users.noreply.github.com>
2024-08-15 17:48:31 +0900

    Mod: JS Vitest tests rewrite in Deno test (with std/assert) - backend framework changing
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/tests/js_denotest/packet_conv/Test_audio.js
+++ src/tests/js_denotest/packet_conv/Test_audio.js
@@ -7,12 +7,12 @@
  *   * Raise RangeError if invalid argument value is passed
  *
  * Test steps:
- *   * Run this script by vitest - `npm test`
+ *   * Run this script by deno test - `deno test **Test*.js`
  *
  * @author aKuad
  */
 
-import { describe, test, expect } from "vitest";
+import { assertEquals, assertThrows } from "jsr:@std/assert@1";
 
 import { ONE_FRAME_SAMPLES } from "../../../static/packet_conv/AUDIO_PARAM.js";
 import { packet_audio_encode, packet_audio_decode, is_audio_packet } from "../../../static/packet_conv/audio.js";
@@ -23,36 +23,36 @@ import { is_almost_equal_float32array } from "../util/almost_eq_f32a.js";
 const ERR_INT16_AND_FLOAT32 = 1 / 32767;
 
 
-describe("true_cases", () => {
-  test("enc_dec_verify_ext", () => {
+Deno.test(async function true_cases(t) {
+  await t.step(function enc_dec_verify_ext() {
     const audio_pcm_org = generate_rand_float32array(ONE_FRAME_SAMPLES);
     const lane_name_org = "ABC";
     const ext_bytes_org = new Uint8Array([1,2,3,4]);
     const raw_packet = packet_audio_encode(audio_pcm_org, lane_name_org, ext_bytes_org);
     const [audio_pcm_prc, lane_name_prc, ext_bytes_prc] = packet_audio_decode(raw_packet);
 
-    expect(is_audio_packet(raw_packet)).toBe(true);
-    expect(is_almost_equal_float32array(audio_pcm_org, audio_pcm_prc, ERR_INT16_AND_FLOAT32)).toBe(true);
-    expect(lane_name_prc).toBe(lane_name_org);
-    expect(ext_bytes_prc).toStrictEqual(ext_bytes_org);
+    assertEquals(is_audio_packet(raw_packet), true);
+    assertEquals(is_almost_equal_float32array(audio_pcm_org, audio_pcm_prc, ERR_INT16_AND_FLOAT32), true);
+    assertEquals(lane_name_prc, lane_name_org);
+    assertEquals(ext_bytes_prc, ext_bytes_org);
   });
 
 
-  test("enc_dec_verify_noext", () => {
+  await t.step(function enc_dec_verify_noext() {
     const audio_pcm_org = generate_rand_float32array(ONE_FRAME_SAMPLES);
     const lane_name_org = "ABC";
     const ext_bytes_org = new Uint8Array();
     const raw_packet = packet_audio_encode(audio_pcm_org, lane_name_org, ext_bytes_org);
     const [audio_pcm_prc, lane_name_prc, ext_bytes_prc] = packet_audio_decode(raw_packet);
 
-    expect(is_audio_packet(raw_packet)).toBe(true);
-    expect(is_almost_equal_float32array(audio_pcm_org, audio_pcm_prc, ERR_INT16_AND_FLOAT32)).toBe(true);
-    expect(lane_name_prc).toBe(lane_name_org);
-    expect(ext_bytes_prc).toStrictEqual(ext_bytes_org);
+    assertEquals(is_audio_packet(raw_packet), true);
+    assertEquals(is_almost_equal_float32array(audio_pcm_org, audio_pcm_prc, ERR_INT16_AND_FLOAT32), true);
+    assertEquals(lane_name_prc, lane_name_org);
+    assertEquals(ext_bytes_prc, ext_bytes_org);
   });
 
 
-  test("enc_dec_verify_silent_ext", () => {
+  await t.step(function enc_dec_verify_silent_ext() {
     const audio_pcm_org = generate_rand_float32array(ONE_FRAME_SAMPLES).map(e => e * 0.1);  // Apply gain -20[dB] = 10 ** (-20/20) = 0.1
     const silent_pcm = new Float32Array(ONE_FRAME_SAMPLES); // Zeros array
     const lane_name_org = "ABC";
@@ -60,14 +60,14 @@ describe("true_cases", () => {
     const raw_packet = packet_audio_encode(audio_pcm_org, lane_name_org, ext_bytes_org);
     const [audio_pcm_prc, lane_name_prc, ext_bytes_prc] = packet_audio_decode(raw_packet);
 
-    expect(is_audio_packet(raw_packet)).toBe(true);
-    expect(audio_pcm_prc).toStrictEqual(silent_pcm);
-    expect(lane_name_prc).toBe(lane_name_org);
-    expect(ext_bytes_prc).toStrictEqual(ext_bytes_org);
+    assertEquals(is_audio_packet(raw_packet), true);
+    assertEquals(audio_pcm_prc, silent_pcm);
+    assertEquals(lane_name_prc, lane_name_org);
+    assertEquals(ext_bytes_prc, ext_bytes_org);
   });
 
 
-  test("enc_dec_verify_silent_noext", () => {
+  await t.step(function enc_dec_verify_silent_noext() {
     const audio_pcm_org = generate_rand_float32array(ONE_FRAME_SAMPLES).map(e => e * 0.1);  // Apply gain -20[dB] = 10 ** (-20/20) = 0.1
     const silent_pcm = new Float32Array(ONE_FRAME_SAMPLES); // Zeros array
     const lane_name_org = "ABC";
@@ -75,14 +75,14 @@ describe("true_cases", () => {
     const raw_packet = packet_audio_encode(audio_pcm_org, lane_name_org, ext_bytes_org);
     const [audio_pcm_prc, lane_name_prc, ext_bytes_prc] = packet_audio_decode(raw_packet);
 
-    expect(is_audio_packet(raw_packet)).toBe(true);
-    expect(audio_pcm_prc).toStrictEqual(silent_pcm);
-    expect(lane_name_prc).toBe(lane_name_org);
-    expect(ext_bytes_prc).toStrictEqual(ext_bytes_org);
+    assertEquals(is_audio_packet(raw_packet), true);
+    assertEquals(audio_pcm_prc, silent_pcm);
+    assertEquals(lane_name_prc, lane_name_org);
+    assertEquals(ext_bytes_prc, ext_bytes_org);
   });
 
 
-  test("verify_ng", () => {
+  await t.step(function verify_ng() {
     const audio_pcm_org = generate_rand_float32array(ONE_FRAME_SAMPLES);
     const lane_name_org = "ABC";
     const ext_bytes_org = new Uint8Array();
@@ -98,43 +98,43 @@ describe("true_cases", () => {
     const raw_packet_invalid_silent_audio_too_short = raw_packet_silent_correct.slice(0, -1);
     const raw_packet_invalid_silent_audio_too_long  = Uint8Array.of(...raw_packet_silent_correct, 0); // 0 as an over length byte
 
-    expect(is_audio_packet("")                                       ).toBe(false); // string "" as non Uint8Array
-    expect(is_audio_packet(raw_packet_invalid_empty)                 ).toBe(false);
-    expect(is_audio_packet(raw_packet_invalid_id)                    ).toBe(false);
-    expect(is_audio_packet(raw_packet_invalid_no_extlen)             ).toBe(false);
-    expect(is_audio_packet(raw_packet_invalid_audio_too_short)       ).toBe(false);
-    expect(is_audio_packet(raw_packet_invalid_audio_too_long)        ).toBe(false);
-    expect(is_audio_packet(raw_packet_invalid_silent_audio_too_short)).toBe(false);
-    expect(is_audio_packet(raw_packet_invalid_silent_audio_too_long) ).toBe(false);
+    assertEquals(is_audio_packet("")                                       , false); // string "" as non Uint8Array
+    assertEquals(is_audio_packet(raw_packet_invalid_empty)                 , false);
+    assertEquals(is_audio_packet(raw_packet_invalid_id)                    , false);
+    assertEquals(is_audio_packet(raw_packet_invalid_no_extlen)             , false);
+    assertEquals(is_audio_packet(raw_packet_invalid_audio_too_short)       , false);
+    assertEquals(is_audio_packet(raw_packet_invalid_audio_too_long)        , false);
+    assertEquals(is_audio_packet(raw_packet_invalid_silent_audio_too_short), false);
+    assertEquals(is_audio_packet(raw_packet_invalid_silent_audio_too_long) , false);
 
-    expect(() => is_audio_packet(""                                       , true)).toThrowError(new TypeError ("raw_packet must be Uint8Array")); // string "" as non Uint8Array
-    expect(() => is_audio_packet(raw_packet_invalid_empty                 , true)).toThrowError(new RangeError("Empty array passed"));
-    expect(() => is_audio_packet(raw_packet_invalid_id                    , true)).toThrowError(new RangeError("It is not an audio packet or silent audio packet"));
-    expect(() => is_audio_packet(raw_packet_invalid_no_extlen             , true)).toThrowError(new RangeError("Too short bytes received, external bytes length missing"));
-    expect(() => is_audio_packet(raw_packet_invalid_audio_too_short       , true)).toThrowError(new RangeError("Too short bytes as audio packet"));
-    expect(() => is_audio_packet(raw_packet_invalid_audio_too_long        , true)).toThrowError(new RangeError("Too long bytes as audio packet"));
-    expect(() => is_audio_packet(raw_packet_invalid_silent_audio_too_short, true)).toThrowError(new RangeError("Too short bytes received, external bytes length missing"));
-    expect(() => is_audio_packet(raw_packet_invalid_silent_audio_too_long , true)).toThrowError(new RangeError("Too long bytes as silent audio packet"));
+    assertThrows(() => is_audio_packet(""                                       , true), TypeError , "raw_packet must be Uint8Array");  // string "" as non Uint8Array
+    assertThrows(() => is_audio_packet(raw_packet_invalid_empty                 , true), RangeError, "Empty array passed");
+    assertThrows(() => is_audio_packet(raw_packet_invalid_id                    , true), RangeError, "It is not an audio packet or silent audio packet");
+    assertThrows(() => is_audio_packet(raw_packet_invalid_no_extlen             , true), RangeError, "Too short bytes received, external bytes length missing");
+    assertThrows(() => is_audio_packet(raw_packet_invalid_audio_too_short       , true), RangeError, "Too short bytes as audio packet");
+    assertThrows(() => is_audio_packet(raw_packet_invalid_audio_too_long        , true), RangeError, "Too long bytes as audio packet");
+    assertThrows(() => is_audio_packet(raw_packet_invalid_silent_audio_too_short, true), RangeError, "Too short bytes received, external bytes length missing");
+    assertThrows(() => is_audio_packet(raw_packet_invalid_silent_audio_too_long , true), RangeError, "Too long bytes as silent audio packet");
   });
 });
 
 
-describe("err_cases", () => {
-  test("enc_invalid_type", () => {
+Deno.test(async function err_cases(t) {
+  await t.step(function enc_invalid_type() {
     const audio_pcm = generate_rand_float32array(ONE_FRAME_SAMPLES);
     const lane_name = "ABC";
     const ext_bytes = new Uint8Array([1,2,3,4]);
 
-    expect(() => packet_audio_encode("", lane_name, ext_bytes)).toThrowError(new TypeError("audio_pcm must be Float32Array"));
-    //                               ~~ as non Float32Array
-    expect(() => packet_audio_encode(audio_pcm,  1, ext_bytes)).toThrowError(new TypeError("lane_name must be string"));
-    //                                           ~ as non string
-    expect(() => packet_audio_encode(audio_pcm, lane_name, "")).toThrowError(new TypeError("ext_bytes must be Uint8Array"));
-    //                                                     ~~ as non Uint8Array
+    assertThrows(() => packet_audio_encode("", lane_name, ext_bytes), TypeError, "audio_pcm must be Float32Array");
+    //                                     ~~ as non Float32Array
+    assertThrows(() => packet_audio_encode(audio_pcm,  1, ext_bytes), TypeError, "lane_name must be string");
+    //                                                 ~ as non string
+    assertThrows(() => packet_audio_encode(audio_pcm, lane_name, ""),  TypeError, "ext_bytes must be Uint8Array");
+    //                                                           ~~ as non Uint8Array
   });
 
 
-  test("enc_invalid_value", () => {
+  await t.step(function enc_invalid_value() {
     const audio_pcm = generate_rand_float32array(ONE_FRAME_SAMPLES);
     const lane_name = "ABC";
     const ext_bytes = new Uint8Array([1,2,3,4]);
@@ -143,25 +143,28 @@ describe("err_cases", () => {
     const lane_name_over_len = "ABCD";
     const ext_bytes_over_len = new Uint8Array(256);
 
-    expect(() => packet_audio_encode(audio_pcm, lane_name_non_ascii, ext_bytes)).toThrowError(new RangeError("For lane_name, non ascii characters are not allowed"));
-    expect(() => packet_audio_encode(audio_pcm, lane_name_over_len , ext_bytes)).toThrowError(new RangeError("For lane_name, over 3 characters string is not allowed"));
-    expect(() => packet_audio_encode(audio_pcm, lane_name, ext_bytes_over_len )).toThrowError(new RangeError("For ext_bytes, over 255 bytes data is not allowed"));
+    assertThrows(() => packet_audio_encode(audio_pcm, lane_name_non_ascii, ext_bytes), RangeError, "For lane_name, non ascii characters are not allowed");
+    assertThrows(() => packet_audio_encode(audio_pcm, lane_name_over_len , ext_bytes), RangeError, "For lane_name, over 3 characters string is not allowed");
+    assertThrows(() => packet_audio_encode(audio_pcm, lane_name, ext_bytes_over_len ), RangeError, "For ext_bytes, over 255 bytes data is not allowed");
   });
 
 
-  // test("dec_invalid_type", () => {
+  // await t.step(function dec_invalid_type() {
   //   // packet verification integrated to `is_audio_packet` tests
   // });
 
-  // test("dec_invalid_value", () => {
+
+  // await t.step(function dec_invalid_value() {
   //   // packet verification integrated to `is_audio_packet` tests
   // });
 
-  // test("verify_invalid_type", () => {
+
+  // await t.step(function verify_invalid_type() {
   //   // no error cases of `is_audio_packet`
   // });
 
-  // test("verify_invalid_value", () => {
+
+  // await t.step(function verify_invalid_value() {
   //   // no error cases of `is_audio_packet`
   // });
 });
--- src/tests/js_denotest/packet_conv/Test_volume_modify.js
+++ src/tests/js_denotest/packet_conv/Test_volume_modify.js
@@ -12,24 +12,25 @@
  * @author aKuad
  */
 
+import { assertEquals, assertThrows } from "jsr:@std/assert@1";
+
 import { packet_volume_modify_encode, packet_volume_modify_decode, is_volume_modify_packet } from "../../../static/packet_conv/volume_modify.js";
-import { describe, test, expect } from "vitest";
 
 
-describe("true_cases", () => {
-  test("enc_dec_verify", () => {
+Deno.test(async function true_cases(t) {
+  await t.step(function enc_dec_verify() {
     const lane_id_org = 1;
     const modified_volume_org = 100;
     const raw_packet = packet_volume_modify_encode(lane_id_org, modified_volume_org);
     const [lane_id_prc, modified_volume_prc] = packet_volume_modify_decode(raw_packet);
 
-    expect(is_volume_modify_packet(raw_packet)).toBe(true);
-    expect(lane_id_prc).toBe(lane_id_org);
-    expect(modified_volume_prc).toBe(modified_volume_org);
+    assertEquals(is_volume_modify_packet(raw_packet), true);
+    assertEquals(lane_id_prc, lane_id_org);
+    assertEquals(modified_volume_prc, modified_volume_org);
   });
 
 
-  test("verify_ng", () => {
+  await t.step(function verify_ng() {
     const lane_id_org = 1;
     const modified_volume_org = 100;
     const raw_packet_correct = packet_volume_modify_encode(lane_id_org, modified_volume_org);
@@ -39,55 +40,55 @@ describe("true_cases", () => {
     const raw_packet_invalid_too_short = raw_packet_correct.slice(0, -1);
     const raw_packet_invalid_too_long  = Uint8Array.of(...raw_packet_correct, 0); // 0 as an over length byte
 
-    expect(is_volume_modify_packet("")                          ).toBe(false);  // string "" as non Uint8Array
-    expect(is_volume_modify_packet(raw_packet_invalid_empty)    ).toBe(false);
-    expect(is_volume_modify_packet(raw_packet_invalid_id)       ).toBe(false);
-    expect(is_volume_modify_packet(raw_packet_invalid_too_short)).toBe(false);
-    expect(is_volume_modify_packet(raw_packet_invalid_too_long) ).toBe(false);
+    assertEquals(is_volume_modify_packet("")                          , false);  // string "" as non Uint8Array
+    assertEquals(is_volume_modify_packet(raw_packet_invalid_empty)    , false);
+    assertEquals(is_volume_modify_packet(raw_packet_invalid_id)       , false);
+    assertEquals(is_volume_modify_packet(raw_packet_invalid_too_short), false);
+    assertEquals(is_volume_modify_packet(raw_packet_invalid_too_long) , false);
 
-    expect(() => is_volume_modify_packet(""                          , true)).toThrowError(new TypeError ("raw_packet must be Uint8Array"));
-    expect(() => is_volume_modify_packet(raw_packet_invalid_empty    , true)).toThrowError(new RangeError("Empty array passed"));
-    expect(() => is_volume_modify_packet(raw_packet_invalid_id       , true)).toThrowError(new RangeError("It is not a volume_modify packet"));
-    expect(() => is_volume_modify_packet(raw_packet_invalid_too_short, true)).toThrowError(new RangeError("Too short bytes as volume modify packet"));
-    expect(() => is_volume_modify_packet(raw_packet_invalid_too_long , true)).toThrowError(new RangeError("Too long bytes as volume modify packet"));
+    assertThrows(() => is_volume_modify_packet(""                          , true), TypeError , "raw_packet must be Uint8Array");
+    assertThrows(() => is_volume_modify_packet(raw_packet_invalid_empty    , true), RangeError, "Empty array passed");
+    assertThrows(() => is_volume_modify_packet(raw_packet_invalid_id       , true), RangeError, "It is not a volume_modify packet");
+    assertThrows(() => is_volume_modify_packet(raw_packet_invalid_too_short, true), RangeError, "Too short bytes as volume modify packet");
+    assertThrows(() => is_volume_modify_packet(raw_packet_invalid_too_long , true), RangeError, "Too long bytes as volume modify packet");
   });
 });
 
 
-describe("err_cases", () => {
-  test("enc_invalid_type", () => {
+Deno.test(async function err_cases(t) {
+  await t.step(function enc_invalid_type() {
     const lane_id = 1;
     const modified_volume = 100;
 
-    expect(() => packet_volume_modify_encode("", modified_volume)).toThrowError(new TypeError("lane_id must be number"));
-    expect(() => packet_volume_modify_encode(lane_id, ""        )).toThrowError(new TypeError("modified_volume must be number"));
+    assertThrows(() => packet_volume_modify_encode("", modified_volume), TypeError, "lane_id must be number");
+    assertThrows(() => packet_volume_modify_encode(lane_id, ""        ), TypeError, "modified_volume must be number");
   });
 
 
-  test("enc_invalid_value", () => {
+  await t.step(function enc_invalid_value() {
     const lane_id = 1;
     const modified_volume = 100;
 
-    expect(() => packet_volume_modify_encode(256, modified_volume)).toThrowError(new RangeError("lane_id must be in 0~255"));
-    expect(() => packet_volume_modify_encode(-1 , modified_volume)).toThrowError(new RangeError("lane_id must be in 0~255"));
-    expect(() => packet_volume_modify_encode(lane_id, 256        )).toThrowError(new RangeError("modified_volume must be in 0~255"));
-    expect(() => packet_volume_modify_encode(lane_id, -1         )).toThrowError(new RangeError("modified_volume must be in 0~255"));
+    assertThrows(() => packet_volume_modify_encode(256, modified_volume), RangeError, "lane_id must be in 0~255");
+    assertThrows(() => packet_volume_modify_encode(-1 , modified_volume), RangeError, "lane_id must be in 0~255");
+    assertThrows(() => packet_volume_modify_encode(lane_id, 256        ), RangeError, "modified_volume must be in 0~255");
+    assertThrows(() => packet_volume_modify_encode(lane_id, -1         ), RangeError, "modified_volume must be in 0~255");
   });
 
 
-  // test("dec_invalid_type", () => {
+  // await t.step(function dec_invalid_type() {
   //   // packet verification integrated to `is_volume_modify_packet` tests
   // });
 
-  // test("dec_invalid_value", () => {
+  // await t.step(function dec_invalid_value() {
   //   // packet verification integrated to `is_volume_modify_packet` tests
   // });
 
-  // test("verify_invalid_type", () => {
+  // await t.step(function verify_invalid_type() {
   //   // no error cases of `is_volume_modify_packet`
   // });
 
-  // test("verify_invalid_value", () => {
+  // await t.step(function verify_invalid_value() {
   //   // no error cases of `is_volume_modify_packet`
   // });
 });
