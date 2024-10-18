76eecea956d256e85f9410221a7dca2561f512f7
aKuad <53811809+aKuad@users.noreply.github.com>
2024-10-18 22:27:21 +0900

    Mod: Depends from `raw_mic_capture.js` to `AUDIO_PARAM.js` cancelled
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/static/raw_mic_capture.js
+++ src/static/raw_mic_capture.js
@@ -6,9 +6,6 @@
  * @author aKuad
  */
 
-import { SAMPLE_RATE } from "./AUDIO_PARAM.js";
-
-
 /**
  * @callback on_frame_ready Callback when sample buffer reached to specified frame length
  * @param {Array<number>} pcm Fetched PCM array at float
@@ -18,11 +15,12 @@ import { SAMPLE_RATE } from "./AUDIO_PARAM.js";
  * Start PCM fetching from mic with callback function
  *
  * @param {on_frame_ready} frame_ready_callback Function for process fetched PCM
- * @param {number} frame_dur_sec Frame duration of callback's argument
+ * @param {number} sample_rate Sample rate of PCM of callback argument
+ * @param {number} frame_dur_sec Frame duration of PCM of callback argument
  * @param {string} processor_path File path of `RawMicCaptureProcessor` defined module
  */
-export async function raw_mic_capture(frame_ready_callback, frame_dur_sec, processor_path) {
-  const audio_ctx = new AudioContext({sampleRate: SAMPLE_RATE});
+export async function raw_mic_capture(frame_ready_callback, sample_rate, frame_dur_sec, processor_path) {
+  const audio_ctx = new AudioContext({sampleRate: sample_rate});
 
   const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
   const source_node = audio_ctx.createMediaStreamSource(stream);
@@ -30,7 +28,7 @@ export async function raw_mic_capture(frame_ready_callback, frame_dur_sec, proce
   await audio_ctx.audioWorklet.addModule(processor_path);
   const capture_node = new AudioWorkletNode(audio_ctx, "raw-mic-capture-processor");
 
-  capture_node.parameters.get("frame_sample_len").value = Math.floor(SAMPLE_RATE * frame_dur_sec);
+  capture_node.parameters.get("frame_sample_len").value = Math.floor(sample_rate * frame_dur_sec);
 
   capture_node.port.start();
   capture_node.port.addEventListener("message", e => {
